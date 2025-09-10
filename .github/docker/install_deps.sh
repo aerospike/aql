39fd996ef1a47320e79c78f3394c2b71ac754723
grant <grant@iowntheinter.net>
2025-09-10 17:08:23 +0000

    import build scripts

---
+++ .github/docker/install_deps.sh
@@ -0,0 +1,90 @@
+#!/usr/bin/env bash
+VERSION=$(git rev-parse HEAD | cut -c -8)
+BUILD_DEPS_REDHAT="readline readline-devel flex which"
+BUILD_DEPS_UBUNTU="libreadline8 libreadline-dev flex"
+BUILD_DEPS_DEBIAN="libreadline8 libreadline-dev flex"
+function install_deps_debian11() {
+  apt -y install $BUILD_DEPS_DEBIAN ruby-rubygems make rpm git snapd curl binutils python3 python3-pip rsync libssl1.1 libssl-dev lzma \
+                 lzma-dev  libffi-dev
+  curl -L https://go.dev/dl/go1.24.6.linux-amd64.tar.gz -o /tmp/go1.24.6.linux-amd64.tar.gz
+  mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-amd64.tar.gz -C /opt/golang
+  /opt/golang/go/bin/go install github.com/asdf-vm/asdf/cmd/asdf@v0.18.0
+  install /root/go/bin/asdf /usr/local/bin/asdf
+  asdf plugin add python https://github.com/asdf-community/asdf-python.git
+  asdf install python 3.10.18
+  asdf set python 3.10.18
+  /root/.asdf/installs/python/3.10.18/bin/python3 -m pip install pipenv
+  install /root/.asdf/installs/python/3.10.18/bin/pipenv /usr/local/bin/pipenv
+  gem install fpm
+}
+
+function install_deps_debian12() {
+  apt -y install $BUILD_DEPS_DEBIAN ruby-rubygems make rpm git snapd curl binutils python3 python3-pip rsync libssl3 libssl-dev lzma \
+                 lzma-dev libffi-dev
+  curl -L https://go.dev/dl/go1.24.6.linux-amd64.tar.gz -o /tmp/go1.24.6.linux-amd64.tar.gz
+  mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-amd64.tar.gz -C /opt/golang
+  /opt/golang/go/bin/go install github.com/asdf-vm/asdf/cmd/asdf@v0.18.0
+  install /root/go/bin/asdf /usr/local/bin/asdf
+  asdf plugin add python https://github.com/asdf-community/asdf-python.git
+  asdf install python 3.10.18
+  asdf set python 3.10.18
+  asdf exec pip install --break-system-packages pipenv
+  gem install fpm
+}
+
+function install_deps_ubuntu20.04() {
+  apt -y install $BUILD_DEPS_UBUNTU ruby make rpm git snapd curl binutils python3 python3-pip rsync libssl1.1 libssl-dev \
+                 lzma lzma-dev libffi-dev
+  curl -L https://go.dev/dl/go1.24.6.linux-amd64.tar.gz -o /tmp/go1.24.6.linux-amd64.tar.gz
+  mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-amd64.tar.gz -C /opt/golang
+  /opt/golang/go/bin/go install github.com/asdf-vm/asdf/cmd/asdf@v0.18.0
+  install /root/go/bin/asdf /usr/local/bin/asdf
+  asdf plugin add python https://github.com/asdf-community/asdf-python.git
+  asdf install python 3.10.18
+  asdf set python 3.10.18
+  asdf exec pip install pipenv
+  gem install fpm
+}
+
+function install_deps_ubuntu22.04() {
+  apt -y install $BUILD_DEPS_UBUNTU ruby-rubygems make rpm git snapd curl binutils python3 python3-pip rsync libssl3 libssl-dev \
+               lzma lzma-dev libffi-dev
+  curl -L https://go.dev/dl/go1.24.6.linux-amd64.tar.gz -o /tmp/go1.24.6.linux-amd64.tar.gz
+  mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-amd64.tar.gz -C /opt/golang
+  /opt/golang/go/bin/go install github.com/asdf-vm/asdf/cmd/asdf@v0.18.0
+  install /root/go/bin/asdf /usr/local/bin/asdf
+  asdf plugin add python https://github.com/asdf-community/asdf-python.git
+  asdf install python 3.10.18
+  asdf set python 3.10.18
+  asdf exec pip install pipenv
+  gem install fpm
+}
+
+function install_deps_ubuntu24.04() {
+  apt -y install $BUILD_DEPS_UBUNTU ruby-rubygems make rpm git snapd curl binutils python3 python3-pip rsync libssl3 libssl-dev \
+               lzma lzma-dev libffi-dev
+  curl -L https://go.dev/dl/go1.24.6.linux-amd64.tar.gz -o /tmp/go1.24.6.linux-amd64.tar.gz
+  mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-amd64.tar.gz -C /opt/golang
+  /opt/golang/go/bin/go install github.com/asdf-vm/asdf/cmd/asdf@v0.18.0
+  install /root/go/bin/asdf /usr/local/bin/asdf
+  asdf plugin add python https://github.com/asdf-community/asdf-python.git
+  asdf install python 3.10.18
+  asdf set python 3.10.18
+  asdf exec pip install --break-system-packages pipenv
+  gem install fpm
+}
+
+function install_deps_redhat-ubi9() {
+  microdnf -y install $BUILD_DEPS_REDHAT ruby rpmdevtools make git python3 python3-pip rsync
+  curl -L https://go.dev/dl/go1.24.6.linux-amd64.tar.gz -o /tmp/go1.24.6.linux-amd64.tar.gz
+  mkdir -p /opt/golang && tar -zxvf /tmp/go1.24.6.linux-amd64.tar.gz -C /opt/golang
+  /opt/golang/go/bin/go install github.com/asdf-vm/asdf/cmd/asdf@v0.18.0
+  install /root/go/bin/asdf /usr/local/bin/asdf
+  asdf plugin add python https://github.com/asdf-community/asdf-python.git
+  microdnf install -y gcc g++ make automake zlib zlib-devel libffi-devel openssl-devel bzip2-devel xz-devel xz xz-libs \
+                      sqlite sqlite-devel sqlite-libs
+  asdf install python 3.10.18
+  asdf set python 3.10.18
+  asdf exec pip install pipenv
+  gem install fpm
+}
