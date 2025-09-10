246ce43c7d38a3bd03111fbca9514cbb23503f7c
grant <grant@iowntheinter.net>
2025-09-10 19:26:30 +0000

    add libtool to redhat build

--- .github/docker/install_deps.sh
+++ .github/docker/install_deps.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 VERSION=$(git rev-parse HEAD | cut -c -8)
-BUILD_DEPS_REDHAT="readline which autoconf" #readline-devel flex
+BUILD_DEPS_REDHAT="readline which autoconf libtool" #readline-devel flex
 BUILD_DEPS_UBUNTU="libreadline8 libreadline-dev flex autoconf libtool"
 BUILD_DEPS_DEBIAN="libreadline8 libreadline-dev flex autoconf libtool"
 function install_deps_debian11() {
