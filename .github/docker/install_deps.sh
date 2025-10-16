0abd9b9ef0c8d1761ea231d9c7ce0e46e3bb235a
grant <grant.haywood@aerospike.com>
2025-10-16 21:51:19 +0000

    standardize rocky linux deps, re-enable redhat 8, pin to specific versions of redhat

--- .github/docker/install_deps.sh
+++ .github/docker/install_deps.sh
@@ -174,8 +174,8 @@ function install_deps_redhat-el8() {
 
 function install_deps_redhat-el9() {
   #todo redhat el9 does not have flex or readline-devel available in the yum repos
-  yum install -y https://rpmfind.net/linux/centos-stream/9-stream/AppStream/$(uname -m)/os/Packages/readline-devel-8.1-4.el9."$(uname -m)".rpm
-  yum install -y https://rpmfind.net/linux/centos-stream/9-stream/AppStream/$(uname -m)/os/Packages/flex-2.6.4-9.el9."$(uname -m)".rpm
+  yum install -y "https://download.rockylinux.org/pub/rocky/9.6/AppStream/$(uname -m)/os/Packages/f/flex-2.6.4-9.el9.$(uname -m).rpm"
+  yum install -y "https://download.rockylinux.org/pub/rocky/9.6/devel/$(uname -m)/os/Packages/r/readline-devel-8.1-4.el9.$(uname -m).rpm"
   dnf -y install $BUILD_DEPS_REDHAT ruby rpmdevtools make git python3 python3-pip rsync
 
   if [ "$(uname -m)" = "x86_64" ]; then
