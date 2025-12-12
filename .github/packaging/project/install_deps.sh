6f037118a345a2310f500328f1b0ca28d20e7da2
Akash Chandra <achandra@aerospike.com>
2025-12-11 20:51:41 -0800

    fix: update install_deps.sh to use vault URLs for Rocky Linux packages

--- .github/packaging/project/install_deps.sh
+++ .github/packaging/project/install_deps.sh
@@ -50,15 +50,15 @@ function install_deps_el8() {
 
 function install_deps_el9() {
   #todo redhat el9 does not have flex or readline-devel available in the yum repos
-  yum install -y "https://download.rockylinux.org/pub/rocky/9.6/AppStream/$(uname -m)/os/Packages/f/flex-2.6.4-9.el9.$(uname -m).rpm"
-  yum install -y "https://download.rockylinux.org/pub/rocky/9.6/devel/$(uname -m)/os/Packages/r/readline-devel-8.1-4.el9.$(uname -m).rpm"
+  yum install -y "https://dl.rockylinux.org/vault/rocky/9.6/AppStream/$(uname -m)/os/Packages/f/flex-2.6.4-9.el9.$(uname -m).rpm"
+  yum install -y "https://dl.rockylinux.org/vault/rocky/9.6/devel/$(uname -m)/os/Packages/r/readline-devel-8.1-4.el9.$(uname -m).rpm"
   dnf -y install $BUILD_DEPS_REDHAT ruby rpmdevtools make git python3 python3-pip rsync
   gem install fpm -v 1.17.0
 }
 
 function install_deps_el10() {
-  yum install -y "https://download.rockylinux.org/pub/rocky/10.0/AppStream/$(uname -m)/os/Packages/f/flex-2.6.4-19.el10.$(uname -m).rpm"
-  yum install -y "https://download.rockylinux.org/pub/rocky/10.0/devel/$(uname -m)/os/Packages/r/readline-devel-8.2-11.el10.$(uname -m).rpm"
+  yum install -y "https://dl.rockylinux.org/vault/rocky/10.0/AppStream/$(uname -m)/os/Packages/f/flex-2.6.4-19.el10.$(uname -m).rpm"
+  yum install -y "https://dl.rockylinux.org/vault/rocky/10.0/devel/$(uname -m)/os/Packages/r/readline-devel-8.2-11.el10.$(uname -m).rpm"
   dnf -y install $BUILD_DEPS_REDHAT ruby rpmdevtools make git python3 python3-pip rsync
   gem install fpm -v 1.17.0
 }
