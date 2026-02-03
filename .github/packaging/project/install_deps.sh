9bf1d9cdda47dd3da62913799f303b83e2a7480f
Akash Chandra <achandra@aerospike.com>
2026-02-02 18:29:19 -0800

    chore: Enhance install_deps.sh to support static libyaml build
    - Added a function to build libyaml from source with static library support for Red Hat and Amazon distributions.
    - Updated installation commands to include curl and ensure proper dependency management across various distributions.
    - Ensured that the static libyaml build is invoked during the installation process for relevant environments.

--- .github/packaging/project/install_deps.sh
+++ .github/packaging/project/install_deps.sh
@@ -1,10 +1,23 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
-BUILD_DEPS_REDHAT="readline which autoconf libtool openssl-devel zlib-devel libyaml-devel"
-BUILD_DEPS_AMAZON="readline which autoconf libtool readline-devel flex openssl-devel zlib-devel libyaml-devel"
+BUILD_DEPS_REDHAT="readline which autoconf libtool openssl-devel zlib-devel"
+BUILD_DEPS_AMAZON="readline which autoconf libtool readline-devel flex openssl-devel zlib-devel"
 BUILD_DEPS_UBUNTU="libreadline8 libreadline-dev flex autoconf libtool libyaml-dev"
 BUILD_DEPS_DEBIAN="libreadline8 libreadline-dev flex autoconf libtool libyaml-dev"
+
+# Build libyaml from source with static library (RHEL/Amazon packages don't include .a files)
+function build_libyaml_static() {
+  local LIBYAML_VERSION="0.2.5"
+  pushd /tmp
+  curl -sL "https://github.com/yaml/libyaml/releases/download/${LIBYAML_VERSION}/yaml-${LIBYAML_VERSION}.tar.gz" | tar xz
+  cd "yaml-${LIBYAML_VERSION}"
+  ./configure --prefix=/usr --enable-static
+  make -j$(nproc)
+  make install
+  popd
+  rm -rf /tmp/yaml-${LIBYAML_VERSION}
+}
 
 function install_deps_debian11() {
   rm -rf /var/lib/apt/lists/*
@@ -77,7 +90,8 @@ function install_deps_el8() {
   dnf module enable -y ruby:2.7
   yum install -y "https://download.rockylinux.org/pub/rocky/8.10/AppStream/$(uname -m)/os/Packages/f/flex-2.6.1-9.el8.$(uname -m).rpm"
   yum install -y "https://download.rockylinux.org/pub/rocky/8.10/Devel/$(uname -m)/os/Packages/r/readline-devel-7.0-10.el8.$(uname -m).rpm"
-  dnf -y install $BUILD_DEPS_REDHAT gcc-c++ ruby rpm-build make git python3 python3-pip rsync wget
+  dnf -y install $BUILD_DEPS_REDHAT gcc-c++ ruby rpm-build make git python3 python3-pip rsync wget curl
+  build_libyaml_static
   gem install fpm -v 1.17.0
   dnf clean all
 }
@@ -88,7 +102,8 @@ function install_deps_el9() {
   #todo redhat el9 does not have flex or readline-devel available in the yum repos
   yum install -y "https://dl.rockylinux.org/vault/rocky/9.6/AppStream/$(uname -m)/os/Packages/f/flex-2.6.4-9.el9.$(uname -m).rpm"
   yum install -y "https://dl.rockylinux.org/vault/rocky/9.6/devel/$(uname -m)/os/Packages/r/readline-devel-8.1-4.el9.$(uname -m).rpm"
-  dnf -y install $BUILD_DEPS_REDHAT ruby rpmdevtools make git python3 python3-pip rsync
+  dnf -y install $BUILD_DEPS_REDHAT ruby rpmdevtools make git python3 python3-pip rsync curl
+  build_libyaml_static
   gem install fpm -v 1.17.0
   dnf clean all
 }
@@ -98,7 +113,8 @@ function install_deps_el10() {
   dnf -y update
   yum install -y "https://dl.rockylinux.org/vault/rocky/10.0/AppStream/$(uname -m)/os/Packages/f/flex-2.6.4-19.el10.$(uname -m).rpm"
   yum install -y "https://dl.rockylinux.org/vault/rocky/10.0/devel/$(uname -m)/os/Packages/r/readline-devel-8.2-11.el10.$(uname -m).rpm"
-  dnf -y install $BUILD_DEPS_REDHAT ruby rpmdevtools make git python3 python3-pip rsync
+  dnf -y install $BUILD_DEPS_REDHAT ruby rpmdevtools make git python3 python3-pip rsync curl
+  build_libyaml_static
   gem install fpm -v 1.17.0
   dnf clean all
 }
@@ -107,7 +123,8 @@ function install_deps_el10() {
 function install_deps_amzn2023() {
   dnf clean all
   dnf -y update
-  dnf -y install $BUILD_DEPS_AMAZON ruby rpmdevtools make git python3 python3-pip rsync
+  dnf -y install $BUILD_DEPS_AMAZON ruby rpmdevtools make git python3 python3-pip rsync curl
+  build_libyaml_static
   gem install fpm -v 1.17.0
   dnf clean all
 }
