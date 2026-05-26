2ca001febfeab4147feb2bd930c4b4ef3c785095
Akash Chandra <achandra@aerospike.com>
2026-05-26 15:03:22 -0700

    ci: TOOLS-3598 Add Ubuntu 26.04 CI and packaging support (#70)

--- .github/bin/install_deps.sh
+++ .github/bin/install_deps.sh
@@ -5,8 +5,8 @@ export FPM_VERSION="1.17.0"
 
 BUILD_DEPS_REDHAT="readline which autoconf libtool openssl-devel zlib-devel"
 BUILD_DEPS_AMAZON="readline which autoconf libtool readline-devel flex openssl-devel zlib-devel"
-BUILD_DEPS_UBUNTU="libreadline8 libreadline-dev flex autoconf automake libtool libyaml-dev zlib1g-dev"
-BUILD_DEPS_DEBIAN="libreadline8 libreadline-dev flex autoconf automake libtool libyaml-dev zlib1g-dev"
+BUILD_DEPS_UBUNTU="libreadline-dev flex autoconf automake libtool libyaml-dev zlib1g-dev"
+BUILD_DEPS_DEBIAN="libreadline-dev flex autoconf automake libtool libyaml-dev zlib1g-dev"
 
 function build_libyaml_static() {
 	local LIBYAML_VERSION="0.2.5"
@@ -79,6 +79,17 @@ function install_deps_ubuntu22.04() {
 }
 
 function install_deps_ubuntu24.04() {
+	rm -rf /var/lib/apt/lists/*
+	apt-get clean
+	apt-get update -o Acquire::Retries=5
+	apt-get install -y --no-install-recommends $BUILD_DEPS_UBUNTU ruby-rubygems make rpm git curl binutils \
+		python3 python3-pip rsync libssl-dev lzma liblzma-dev libffi-dev build-essential ruby-dev
+	gem install fpm -v "$FPM_VERSION"
+	apt-get clean
+	rm -rf /var/lib/apt/lists/* /var/cache/apt/archives/*
+}
+
+function install_deps_ubuntu26.04() {
 	rm -rf /var/lib/apt/lists/*
 	apt-get clean
 	apt-get update -o Acquire::Retries=5
